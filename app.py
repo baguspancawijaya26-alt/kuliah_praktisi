4be9fb372b3f6e2e7f1ed4ea95f390b2a9c392f1
baguspancawijaya26-alt <baguspancawijaya26@gmail.com>
2026-05-23 11:16:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title="Finance Dashboard",
-  layout="wide"
+  page_title="Kuliah Praktisi 2305",
+  page_icon="🧊",
+  layout="centered",
+  initial_sidebar_state="expanded"
 )
 
 # Hirarki teks
@@ -10,17 +12,38 @@ st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
+st.write("Hello, *World!* 😁")
 
-# Inisialisasi session_state
-if "authenticated" not in st.session_state:
-    st.session_state.authenticated = False
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
 
-# Login check
-if st.button("Login"):
-    if USERS.get(username) == password:
-        st.session_state.authenticated = True
-        st.session_state.username = username
-        st.rerun()
-    else:
-        st.error("Invalid credentials")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
+st.button("Reset", type="primary")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
